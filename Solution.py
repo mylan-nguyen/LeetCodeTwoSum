ed8ef4389b7eca90242e4c2c073e038ea1652e8e
Mylan Nguyen <mylannguyen482@gmail.com>
2022-05-09 21:21:16 -0400

    Updated Solution.py

--- Solution.py
+++ Solution.py
@@ -1,37 +1,4 @@
 class Solution(object):
-    @staticmethod
-    def twoSum(nums, target):
-        # your code goes here
-        indexes = []
-        for num in nums:
-            print("i am here at this num", num)
-            for num2 in nums:
-                #if num == num2:
-
-                print("num2: ", num2)
-                check = nums.index(num)
-                print('the num is: ', num, "and it's index is: ", check)
-                check2 = nums.index(num2)
-                print('the num2 is: ', num2, "and it's index is: ", check2)
-                if check != check2:
-                    two_sum = num + num2
-                    print("the sum of ", num, " and ", num2, " is ", two_sum)
-                    print("The target is ", target)
-                    if num + num2 == target:
-                        print("FOUND A PAIR: ", num, " and ", num2)
-                        num_index = nums.index(num)
-                        num2_index = nums.index(num2)
-                        print(num_index)
-                        print(num2_index)
-                        indexes.append(num_index)
-                        indexes.append(num2_index)
-                        print(indexes)
-                print()
-
-        print('My current index list looks: ', indexes)
-        unique_list = []
-        [unique_list.append(n) for n in indexes if n not in unique_list]
-        return unique_list
 
     @staticmethod
     def twoSumNew(nums, target):
@@ -59,16 +26,8 @@ def main():
     target1 = 6
     print("Answer", Solution.twoSumNew(nums1, target1))
 
-    # nums = [3, 3, 2, 4]
     nums = [3, 3]
     target = 6
     print("Answer", Solution.twoSumNew(nums, target))
-
-
-
-
-
-
-
 
 main()
